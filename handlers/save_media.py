e0c4e18772c57dbae0766dffc981560a8ba0b566
Kazutora <mr.nishan.bst@gmail.com>
2023-04-13 10:30:57 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -94,8 +94,8 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             "Just Click the link to get your file!",
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton("Open Link", url=share_link)],
-                 [InlineKeyboardButton("Bots Channel", url="https://t.me/TeleRoidGroup"),
-                  InlineKeyboardButton("Support Group", url="https://t.me/TeleRoid14")]]
+                 [InlineKeyboardButton("Anime Channel", url="https://t.me/Saga_Anime"),
+                  InlineKeyboardButton("Request Group", url="https://t.me/Valhaalla006")]]
             ),
             disable_web_page_preview=True
         )
